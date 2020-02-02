db09f371e1030c7e1d0948cf604f92de73a49b78
Victor Puntel <victorpuntel@gmail.com>
2020-02-02 14:37:49 -0300

    Added ESLint and Prettier

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ foo: 'bar' });
+  return res.json({ foo: 'bar' });
 });
 
 export default routes;
